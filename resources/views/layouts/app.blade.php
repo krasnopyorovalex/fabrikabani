915bb4344306e84399a3391f5db42a95649ccbfc
sanek <krasnopyorovalex88@gmail.com>
2022-02-06 14:37:38 +0300

    add mail.ru counter

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -120,36 +120,6 @@
                                             <li><a class="icon mdi mdi-instagram" href="https://instagram.com/fabrikabani_sev?igshid=8ofb61cmbv8v" target="_blank"></a></li>
                                         </ul>
                                     </div>
-                                    <div>
-                                        <!-- Rating Mail.ru counter -->
-                                        <script type="text/javascript">
-                                            var _tmr = window._tmr || (window._tmr = []);
-                                            _tmr.push({id: "3237300", type: "pageView", start: (new Date()).getTime(), pid: "USER_ID"});
-                                            (function (d, w, id) {
-                                                if (d.getElementById(id)) return;
-                                                var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
-                                                ts.src = "https://top-fwz1.mail.ru/js/code.js";
-                                                var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
-                                                if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
-                                            })(document, window, "topmailru-code");
-                                        </script><noscript><div>
-                                                <img src="https://top-fwz1.mail.ru/counter?id=3237300;js=na" style="border:0;position:absolute;left:-9999px;" alt="Top.Mail.Ru" />
-                                            </div></noscript>
-                                        <!-- //Rating Mail.ru counter -->
-
-                                        <!-- Rating@Mail.ru counter dynamic remarketing appendix -->
-                                        <script type="text/javascript">
-                                            var _tmr = _tmr || [];
-                                            _tmr.push({
-                                                type: 'itemView',
-                                                productid: 'VALUE',
-                                                pagetype: 'VALUE',
-                                                list: 'VALUE',
-                                                totalvalue: 'VALUE'
-                                            });
-                                        </script>
-                                        <!-- // Rating@Mail.ru counter dynamic remarketing appendix -->
-                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -261,6 +231,34 @@
 
         gtag('config', 'G-TYE7MQ1JWY');
     </script>
+    <!-- Rating Mail.ru counter -->
+    <script type="text/javascript">
+        var _tmr = window._tmr || (window._tmr = []);
+        _tmr.push({id: "3237300", type: "pageView", start: (new Date()).getTime(), pid: "USER_ID"});
+        (function (d, w, id) {
+            if (d.getElementById(id)) return;
+            var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+            ts.src = "https://top-fwz1.mail.ru/js/code.js";
+            var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+            if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+        })(document, window, "topmailru-code");
+    </script><noscript><div>
+            <img src="https://top-fwz1.mail.ru/counter?id=3237300;js=na" style="border:0;position:absolute;left:-9999px;" alt="Top.Mail.Ru" />
+        </div></noscript>
+    <!-- //Rating Mail.ru counter -->
+
+    <!-- Rating@Mail.ru counter dynamic remarketing appendix -->
+    <script type="text/javascript">
+        var _tmr = _tmr || [];
+        _tmr.push({
+            type: 'itemView',
+            productid: 'VALUE',
+            pagetype: 'VALUE',
+            list: 'VALUE',
+            totalvalue: 'VALUE'
+        });
+    </script>
+    <!-- // Rating@Mail.ru counter dynamic remarketing appendix -->
     @endif
 </body>
 </html>
